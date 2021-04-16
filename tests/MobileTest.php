f191dac0c54983c70a1898be0fc9026bf22010af
Andrés Añez <aeanez@gmail.com>
2021-04-16 18:18:17 -0500

    Adding test method to make a call with a valid contact

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Call;
 use App\Mobile;
 use Mockery as m;
 use App\Providers\MyProvider;
@@ -18,6 +19,21 @@ class MobileTest extends TestCase
 		$mobile = new Mobile($provider);
 
 		$this->assertNull($mobile->makeCallByName(''));
+	}
+
+	/** @test */
+	public function it_returns_a_call_when_have_a_valid_contact()
+	{
+		$mockProvider = m::mock(MyProvider::class);
+		$mobile = new mobile($mockProvider);
+		$mockMobile = m::mock($mobile);
+
+		$mockMobile->shouldReceive('makeCallByName')
+				   ->andReturn(new Call());
+
+		$call = $mockMobile->makeCallByName('Andres');
+
+		$this->assertEquals(Call::class, get_class($call));
 	}
 
 }
